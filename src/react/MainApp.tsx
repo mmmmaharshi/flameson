0a165a31613457f07f1d870a02ebe650e829b5ea
Manohar-Maharshi <netwitch1012@gmail.com>
2024-12-20 00:39:36 +0530

    fix: :bug: flames result message in share options

--- src/react/MainApp.tsx
+++ src/react/MainApp.tsx
@@ -24,7 +24,7 @@ const MainApp: React.FC = () => {
       navigator
         .share({
           title: "FLAMEson",
-          text: "resultMessage",
+          text: resultMessage,
           url: window.location.href,
         })
         .then(() => console.log("Shared successfully!"))
